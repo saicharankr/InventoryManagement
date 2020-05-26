bdac33090a3264d69d3e57d4fffd80269590986d
saicharan <saicharan@celominds.com>
2020-05-26 16:06:16 +0530

    Adding Swagger

--- InventoryProject/Apis/LoginApiController.cs
+++ InventoryProject/Apis/LoginApiController.cs
@@ -1,12 +1,88 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InventoryProject.Areas.Identity.Pages.Account;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace InventoryProject.Apis
 {
     public class LoginApiController : Controller
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private IConfiguration _config;
+
+        public LoginApiController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager, IConfiguration config)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _config = config;
+        }
+
+        [Route("login")]
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] string emailaddress, string password)
+        {
+            try
+            {
+                //Sign user in with username and password from parameters. This code assumes that the emailaddress is being used as the username.
+                var result = await _signInManager.PasswordSignInAsync(emailaddress, password, false, false);
+
+                if (result.Succeeded)
+                {
+                    //Retrieve authenticated user's details
+                    var user = await _userManager.FindByEmailAsync(emailaddress);
+
+                    //Generate unique token with user's details
+                    var tokenString = await GenerateJSONWebToken(user);
+
+                    //Return Ok with token string as content
+                    return Ok(new { token = tokenString });
+                }
+                return Unauthorized();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        private async Task<string> GenerateJSONWebToken(IdentityUser user)
+        {
+            //Hash Security Key Object from the JWT Key
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            //Generate list of claims with general and universally recommended claims
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.Id)
+            };
+            //Retreive roles for user and add them to the claims listing
+            var roles = await _userManager.GetRolesAsync(user);
+            claims.AddRange(roles.Select(r => new Claim(ClaimsIdentity.DefaultRoleClaimType, r)));
+            //Generate final token adding Issuer and Subscriber data, claims, expriation time and Key
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"]
+                , _config["Jwt:Issuer"],
+                claims,
+                null,
+                expires: DateTime.Now.AddHours(5),
+                signingCredentials: credentials
+            );
+
+            //Return token string
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
